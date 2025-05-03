a82859ae6a9066cd6a505303331e921c34590067
Bhumit Tanwar <bhumittanwar2006@gmail.com>
2025-05-03 16:52:44 +0530

    Add files via upload

---
+++ Courses.py
@@ -0,0 +1,48 @@
+# Data Science Courses
+ds_course = [
+    ("Data Science Full Course for Beginners (2025 Edition)", "https://www.youtube.com/watch?v=gDZ6czwuQ18"),
+    ("Data Science Full Course for Beginners - Edureka", "https://www.youtube.com/watch?v=-ETQ97mXXF0"),
+    ("Data Science Full Course - Codebasics Playlist", "https://www.youtube.com/playlist?list=PLeo1K3hjS3us_ELKYSj_Fth2tIEkdKXvV")
+]
+
+# Web Development Courses
+web_course = [
+    ("Full Stack Web Development Course (22 Hours)", "https://www.youtube.com/watch?v=ZxKM3DCV2kE"),
+    ("Web Development Full Course 2025 - Simplilearn", "https://www.youtube.com/watch?v=1-Wgl_Cj2Ok"),
+    ("Web Development Tutorials - CodeWithHarry", "https://www.youtube.com/playlist?list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n")
+]
+
+# Android Development Courses
+android_course = [
+    ("Android Development for Beginners - FreeCodeCamp", "https://www.youtube.com/watch?v=fis26HvvDII"),
+    ("Android Development Full Course - Simplilearn", "https://www.youtube.com/watch?v=1iaimCQHeHw"),
+    ("Android App Development Playlist - Simplilearn", "https://www.youtube.com/playlist?list=PLEiEAq2VkUULG4j7aDpamb6b5Ym3RFQOj")
+]
+
+# iOS Development Courses
+ios_course = [
+    ("Complete iOS Developer Course", "https://www.youtube.com/watch?v=rp56yKfIkmo"),
+    ("Swift Programming Tutorial | FULL COURSE", "https://www.youtube.com/watch?v=CwA1VWP0Ldw"),
+    ("iOS App Full Course | Rick & Morty App", "https://www.youtube.com/watch?v=fTGA8cjbf5Y")
+]
+
+# UI/UX Design Courses
+uiux_course = [
+    ("UI/UX Design Full Course - Intellipaat", "https://www.youtube.com/watch?v=BU_afT-aIn0"),
+    ("FREE UX/UI DESIGN COURSE - Adobe XD", "https://www.youtube.com/playlist?list=PLCzG4PAUTlDq9KcmLvkrdbOxJNFBSSYnH"),
+    ("UI/UX Crash Course for Beginners - Figma", "https://www.youtube.com/watch?v=cvm_ECH94x4")
+]
+
+# Resume Writing Videos
+resume_videos = [
+    "https://www.youtube.com/watch?v=z9oEbG1GhqM",
+    "https://www.youtube.com/watch?v=Tt08KmFfIYQ",
+    "https://www.youtube.com/watch?v=vpmOQfdMtqA"
+]
+
+# Interview Preparation Videos
+interview_videos = [
+    "https://www.youtube.com/watch?v=y0gIQs40KOo",
+    "https://www.youtube.com/watch?v=-LVmKNvflnY",
+    "https://www.youtube.com/watch?v=8BSYUWgn4Ig"
+]
